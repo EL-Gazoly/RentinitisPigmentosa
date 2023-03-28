4905ae11f66cff79e3d6d54714444c02fe4a6f76
Adham ElGazoly <elgazoly11@gmail.com>
2023-03-28 02:48:23 +0200

    add customized fonts and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,14 +6,19 @@ module.exports = {
       sans: ['Open Sans', 'sans-serif'],
       roboto: ['Roboto', 'sans-serif'],
       nunito: ['Nunito', 'sans-serif'],
+      poppins : ['Poppins', 'sans-serif'],
     },
     extend: {
+      rotate :{
+        '270' : '270deg',
+      },
       colors: {
         primary: '#0072C6',
         bg: '#F5F5F5',
         secondary: '#838AA3',
         border : '#B2D4ED',
         line: '#4C9CD7',
+        upload : '#846e33',
       },
     },
   },
