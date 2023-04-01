c403b81857a92d54a8eb391f74ebde66ab0fcbee
Adham ElGazoly <elgazoly11@gmail.com>
2023-04-01 20:09:27 +0200

    Add form field validation to SignUp page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 import usePost from '../hooks/usePost'
-import {React, useRef} from 'react'
+import {React, useRef, useState} from 'react'
 import PageLogo from '../components/PageLogo'
 import Loading from '../components/Loading'
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +11,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const SignUp = () => {
 
+  const [firstNameOk, setFirstNameOk] = useState(true);
+  const [lastNameOk, setLastNameOk] = useState(true);
+  const [emailOk, setEmailOk] = useState(true);
+  const [passwordOk, setPasswordOk] = useState(true);
+  
 
   const firstname = useRef();
   const lastname = useRef();
@@ -40,6 +45,47 @@ const SignUp = () => {
       navigate('/login');
     }, 1700);
   };
+
+  const handelFirstNameChangeCheck = () => {
+    const name = firstname.current.value;
+    if (name.length < 3) {
+      setFirstNameOk(true);
+    }
+    else {
+      setFirstNameOk(false);
+    }
+  };
+
+  const handelLastNameChangeCheck = () => {
+    const name = lastname.current.value;
+    if (name.length < 3) {
+      setLastNameOk(true);
+    }
+    else {
+      setLastNameOk(false);
+    }
+  };
+
+  const handelEmailChangeCheck = () => {
+    const emailValue = email.current.value;
+    if (!/\S+@\S+\.\S+/.test(emailValue)) {
+      setEmailOk(true);
+    }
+    else {
+      setEmailOk(false);
+    }
+  };
+
+  const handelPasswordChangeCheck = () => {
+    const passwordValue = password.current.value;
+    if (passwordValue.length < 8 || !/\d/.test(passwordValue) || !/[a-z]/.test(passwordValue) || !/[A-Z]/.test(passwordValue)) {
+      setPasswordOk(true);
+    }
+    else {
+      setPasswordOk(false);
+    }
+  };
+
    
   return (
     <div>
@@ -62,28 +108,41 @@ const SignUp = () => {
             <div className="form grid grid-cols-1 gap-y-8 md:md:gap-11 xl:ml-12 2xl:ml-32 ">
                 <h3 className=' self-start text-3xl font-nunito font-bold caret-transparent ml-7 md:ml-5 md:text-4xl'>Sign up.</h3>
                 <div className="form-group self-center flex flex-col gap-y-4 w-11/12 ml-4  caret-primary md:gap-10 xl:w-4/5 2xl:w-3/4">
-                    <input type="text" placeholder='firstname' className='h-12 bg-bg border-b-2 rounded-md pl-4 pt-1 
-                     border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary '
+                    <input type="text" placeholder='firstname' className={`h-12 bg-bg border-b-2 rounded-md pl-4 pt-1 
+                     border-gray-400 focus:outline-none placeholder:text-gray-400 
+                      ${firstNameOk ? 'focus:border-red-500' : 'focus:border-primary'}
+                     `}
                      ref={firstname}
+                     onChange={handelFirstNameChangeCheck}
                      />
 
-                    <input type="text" placeholder='lastname' className='h-12 bg-bg border-b-2 rounded-md pl-4 pt-1 
-                     border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary '
+                    <input type="text" placeholder='lastname' className={`h-12 bg-bg border-b-2 rounded-md pl-4 pt-1 
+                     border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary 
+                      ${lastNameOk ? 'focus:border-red-500' : 'focus:border-primary'}
+                     `}
                         ref={lastname}
+                        onChange={handelLastNameChangeCheck}
                      />
 
-                    <input type="email" placeholder='email' className='h-12 bg-bg border-b-2 rounded-md pl-4 pt-1
-                      border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary '
+                    <input type="email" placeholder='email' className={`h-12 bg-bg border-b-2 rounded-md pl-4 pt-1
+                      border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary
+                      ${emailOk ? 'focus:border-red-500' : 'focus:border-primary'}
+                      `}
+                        onChange={handelEmailChangeCheck}
                         ref={email}
                       />
                       
-                    <input type="password" placeholder='password' className='  h-12 bg-bg border-b-2 rounded-md pl-4 pt-1
-                      border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary ' 
+                    <input type="password" placeholder='password' className={`  h-12 bg-bg border-b-2 rounded-md pl-4 pt-1
+                      border-gray-400 focus:outline-none placeholder:text-gray-400 focus:border-primary 
+                      ${passwordOk ? 'focus:border-red-500' : 'focus:border-primary'}
+                      ` }
+                        onChange={handelPasswordChangeCheck}
                         ref={password}
                       />
             </div>
                 <div className=" caret-transparent form-button self-end flex flex-col gap-y-5 text-center w-11/12  ml-4 xl:ml-0 xl:w-4/5 ">
-                    <button  onClick={handleSubmit} className='  h-11 bg-primary flex justify-center items-center text-white font-nunito text-xl  rounded-thiry xl:ml-16 2xl:ml-6' >Sign up</button>
+                    <button  onClick={handleSubmit} className='  h-11 bg-primary flex justify-center items-center text-white font-nunito text-xl  rounded-thiry xl:ml-16 2xl:ml-6'
+                    >Sign up</button>
                     <span className=' text-primary text-sm  font-roboto font-medium' >Already have an account? <Link to="/login" className=' text-primary text-base font-roboto font-medium underline'>Login</Link></span>
                 </div>
             </div>
