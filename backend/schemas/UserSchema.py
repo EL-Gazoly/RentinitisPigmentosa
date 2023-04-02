59c92c7e833e376b89cb63482303ab9740392b33
Adham ElGazoly <elgazoly11@gmail.com>
2023-04-02 20:43:22 +0200

    Remove validation for password length, uppercase, lowercase, and digit requirements in LoginUser

--- backend/schemas/UserSchema.py
+++ backend/schemas/UserSchema.py
@@ -39,17 +39,6 @@ class LoginUser(BaseModel):
     email: EmailStr
     password: str
 
-    @validator('password')
-    def validate_password(cls, v):
-        if len(v) < 8:
-            raise ValueError("Password must be at least 8 characters long")
-        if not re.search(r'\d', v):
-            raise ValueError("Password must contain at least one digit")
-        if not re.search(r'[A-Z]', v):
-            raise ValueError("Password must contain at least one uppercase letter")
-        if not re.search(r'[a-z]', v):
-            raise ValueError("Password must contain at least one lowercase letter")
-        return v
 
 class ForgetPassword(BaseModel):
     email: EmailStr
