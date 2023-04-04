46cc6e2a84142c6021723a62813467faec729f46
Adham ElGazoly <elgazoly11@gmail.com>
2023-04-04 16:13:53 +0200

    adjust to toggle by clicking anywhere in the div

--- frontend/src/components/HighContrastButton.jsx
+++ frontend/src/components/HighContrastButton.jsx
@@ -8,15 +8,16 @@ const HighContrastButton = ({onToggle}) => {
         onToggle()
     }
   return (
-    <div className='fixed bottom-0 left-0 p-4 cursor-pointer'>
+    <div className='fixed bottom-0 left-0 p-4 cursor-pointer'
+    onClick={() => toggle()}
+    >
       <div className="relative inline-block w-16 h-8 bg-gray-400 rounded-full">
       <button
         className={`absolute left-0 top-0 w-8 h-8 bg-white rounded-full shadow-md transform transition-transform ease-in-out duration-300 ${
           isToggle ? "translate-x-full bg-green-500" : ""
         }`}
-        onClick={() => toggle()}
+       
       >
-        
          <span className={`absolute right-0 top-0 h-full flex items-center px-2 text-xs font-bold text-gray-600 ${isToggle ? 'text-white pr-3' : ''}
           
         `}>
