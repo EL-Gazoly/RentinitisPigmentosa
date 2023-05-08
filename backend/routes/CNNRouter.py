6c2ad8bded5cd98e216ce03e4c5e86cd0b9f6cf2
Adham ElGazoly <elgazoly11@gmail.com>
2023-05-09 00:22:46 +0300

    connect the backend to the tesnflow model

--- backend/routes/CNNRouter.py
+++ backend/routes/CNNRouter.py
@@ -1,6 +1,10 @@
 from typing import List
 # from fastapi_jwt_auth import AuthJWT
 from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
+import os
+import shutil
+import pickle
+from tfModel.classification_for_web import SingleImage_Test
 
 CNNRouter = APIRouter()
 
@@ -9,7 +13,12 @@ async def UploadImg(files: List[UploadFile] = File(...)):
     
     try:
         for file in files:
-            print(file.filename)
-        return {'message': 'Files uploaded'}
+            file_path = os.path.join(os.getcwd(), "uploads", file.filename)
+            with open(file_path, "wb") as buffer:
+                shutil.copyfileobj(file.file, buffer)
+                
+            print(file_path)
+        return SingleImage_Test(file_path)
     except Exception as e:
         raise HTTPException(status_code=500, detail='An internal error occuered')
+ 
